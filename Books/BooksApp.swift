e0dc640340e8cf09bfab5f46c87deb25d1b908a9
Scastaneda98 <santiagocastanedarestrepo@gmail.com>
2024-02-15 18:06:27 -0500

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Santiago Castaneda on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
